54f63e1b34a5c20441f27992b5e4d6e2f0fabb57
Lucas4545ah <52332868+Lucas4545ah@users.noreply.github.com>
2022-03-06 15:02:19 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+if (args[0] == "write")
+{
+    Console.ForegroundColor = (ConsoleColor)int.Parse(args[1]);
+    Console.BackgroundColor = (ConsoleColor)int.Parse(args[2]);
+    List<string> list = new List<string>();
+    for (int i = 3; i < args.Count(); i++)
+    {
+        list.Add(args[i]);
+    }
+
+    Console.Write(string.Join(" ", list.ToArray()));
+}
+if (args[0] == "writeline")
+{
+    Console.ForegroundColor = (ConsoleColor)int.Parse(args[1]);
+    Console.BackgroundColor = (ConsoleColor)int.Parse(args[2]);
+    List<string> list = new List<string>();
+    for (int i = 3; i < args.Count(); i++)
+    {
+        list.Add(args[i]);
+    }
+
+    Console.WriteLine(string.Join(" ", list.ToArray()));
+}
